c08e443120a5c20c5bf3f58ae68389511f972b17
Daan Vos de Wael <daan@vosdewael.com>
2022-04-06 15:01:14 +0200

    feature: init commit

---
+++ data/memes.js
@@ -0,0 +1,16 @@
+const memes = [
+    {
+        id: 1,
+        filename: 'emperors-new-groove.webp',
+        title: "Emperor's new groove",
+        keywords: ['emperor', 'new', 'groove', 'pacha', 'feels', 'good'],
+    },
+    {
+        id: 2,
+        filename: 'shai-holl-up.webp',
+        title: 'Holl up Shailin',
+        keywords: ['holl', 'up', 'shailin'],
+    },
+];
+
+export default memes;
