cbafaca11ad2fddef29c1695ce7b35619a38b184
Jochem <info@jochemkeller.nl>
2022-06-21 17:22:38 +0200

    chore: improve tsconfig

--- workflow/utilities/tags.ts
+++ workflow/utilities/tags.ts
@@ -2,6 +2,8 @@ import { readFile, writeFile } from 'node:fs/promises';
 
 import { SupabaseClient } from '@supabase/supabase-js';
 
+import { Image } from '@/utilities/images';
+import { Meme } from '@/utilities/memes';
 import { filterTags, queryTags, Tag } from '@/utilities/tags';
 
 export const downloadTagsJSON = async ({ $supabase }: { $supabase: SupabaseClient }): Promise<void> => {
@@ -26,4 +28,17 @@ export const getTags = async ({ query }: { query: string }): Promise<number[]> =
         query,
         tags,
     });
+};
+
+export const getTagsStringForImageId = async ({ id } : { id: Image['id'] }): Promise<string> => {
+    const tagsRaw = await readFile('./.cache/tags.json', { encoding: 'utf8' });
+    const memesRaw = await readFile('./.cache/memes.json', { encoding: 'utf8' });
+
+    const tags: Tag[] = JSON.parse(tagsRaw);
+    const memes: Meme[] = JSON.parse(memesRaw);
+
+    const foundMemes = memes.filter(meme => meme.image_id === id);
+    const foundTags = foundMemes.map(meme => tags.find(tag => tag.id === meme.tag_id).name);
+
+    return `Tags: ${foundTags.join(', ')}`;
 };
