068182884a40a98c7de9c8a588d9cf17658a5ba1
Jochem <info@jochemkeller.nl>
2022-06-27 14:18:22 +0200

    feat: responsive improvements & stylelint

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,5 +1,6 @@
 export default {
     '*.*': filenames => [`case-police  --fix ${filenames.join(' ')}`],
+    '*.{css,vue}': filenames => [`stylelint ${filenames.join(' ')}`],
     '*.{json,mjs,ts,vue}': filenames => [
         `eslint ${filenames.join(' ')}`,
         `vitest ${filenames.join(' ')} --passWithNoTests`,
