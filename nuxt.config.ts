0b0abcf500c3d105f335ebc17e45d5755c087468
Jochem <info@jochemkeller.nl>
2022-06-27 17:38:12 +0200

    feat: add nuxt pwa plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,37 @@ export default defineNuxtConfig({
     modules: [
         '@nuxtjs/supabase',
         '@pinia/nuxt',
+        '@kevinmarrec/nuxt-pwa',
         '@unocss/nuxt',
     ],
     postcss: {
         plugins: {
             'postcss-nested': {},
             'postcss-nested-ancestors': {},
+        },
+    },
+    pwa: {
+        icon: {
+            sizes: [
+                192,
+                512,
+            ],
+            source: 'public/icon.png',
+        },
+        manifest: {
+            background_color: '#C39E29', // eslint-disable-line camelcase
+            description: 'Internet\'s Zuurste Memes',
+            name: 'Zure Memes',
+            short_name: 'Zure Memes', // eslint-disable-line camelcase
+            theme_color: '#C39E29', // eslint-disable-line camelcase
+        },
+        meta: {
+            author: '@zeebats',
+            description: 'Internet\'s Zuurste Memes',
+            favicon: false,
+            mobileAppIOS: true,
+            name: '',
+            theme_color: '#C39E29', // eslint-disable-line camelcase
         },
     },
     unocss: {
