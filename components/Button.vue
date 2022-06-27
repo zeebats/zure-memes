b8ef513358f4603ba16aa84f56e7f9636883e3a2
Jochem <info@jochemkeller.nl>
2022-06-27 17:35:24 +0200

    feat: improve ios mobile ux

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-    <button class="inline-flex items-baseline justify-center focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5">
+    <button class="inline-flex items-baseline justify-center focus:outline-none focus:ring-4 font-medium rounded-lg text-base px-5 py-2.5">
         <slot />
     </button>
 </template>
