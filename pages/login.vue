38e7b4bfd0da17b564df31f256c69eead015b26b
Jochem <info@jochemkeller.nl>
2022-06-28 09:10:53 +0200

    fix: a11y & pwa sizing

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid min-h-screen place-content-center">
+    <div class="grid min-h-screen min-h-[100dvh] place-content-center">
         <button
             type="button"
             class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-2 mb-2"
